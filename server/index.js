cfc5455e670c6195b8b91661890f94818d9f9d55
Felix La Rocque Carrier <felixlrc21@gmail.com>
2018-11-10 18:30:44 -0500

    Add DB connexion

--- server/index.js
+++ server/index.js
@@ -2,16 +2,15 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const helmet = require('helmet');
-const proxyMiddleware = require('http-proxy-middleware');
 const path = require('path');
 const next = require('next');
 const i18nextMiddleware = require('i18next-express-middleware');
 const Backend = require('i18next-node-fs-backend');
+const Sequelize = require('sequelize');
 const i18nextNamespaces = require('../app/utils/i18nextNamespaces');
-
+const api = require('./lib/api');
 
 const dev = process.env.NODE_ENV !== 'production';
-let devProxy;
 if (dev) {
   dotenv.config();
 }
@@ -68,12 +67,39 @@ i18n
         // missing keys
         server.post('/locales/add/:lng/:ns', i18nextMiddleware.missingKeyHandler(i18n));
 
-        // Set up the proxy.
-        if (dev && devProxy) {
-          Object.keys(devProxy).forEach((context) => {
-            server.use(proxyMiddleware(context, devProxy[context]));
+        // Database
+        const sequelize = new Sequelize(process.env.DATABASE_URL, {
+          dialect: 'postgres',
+          dialectOptions: {
+            ssl: true,
+          },
+        });
+
+        sequelize
+          .authenticate()
+          .then(() => {
+            console.log('Connection has been established successfully.'); // eslint-disable-line no-console
+          })
+          .catch((err) => {
+            console.error('Unable to connect to the database:', err); // eslint-disable-line no-console
           });
-        }
+
+        const Theme = sequelize.define('theme', {
+          nameFr: {
+            type: Sequelize.STRING,
+          },
+          nameEN: {
+            type: Sequelize.STRING,
+          },
+        });
+
+        Theme.sync({ force: true }).then(() => Theme.create({
+          nameFr: 'Islande',
+          nameEN: 'Iceland',
+        }));
+
+        // Set API
+        server.use('/api', api(sequelize));
 
         server.get('*', (req, res) => handle(req, res));
 
