20e4de6a6438a3ad5f606ca97e54dd8a273b99e8
Felix La Rocque Carrier <felixlrc21@gmail.com>
2019-01-07 22:23:31 -0500

    Update Ads

--- app/ads/sideDestinationPageAd.js
+++ app/ads/sideDestinationPageAd.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import AD from 'react-google-publisher-tag';
 
-const SideDestinationPageAd = () => <AD path="/21784165674/ZTEST_ads" id="destination-GAM" />;
+const SideDestinationPageAd = () => (
+  <AD path="/21784165674/destination_side_ad" id="div-gpt-ad-1546917788906-0" />
+);
 
 export default SideDestinationPageAd;
