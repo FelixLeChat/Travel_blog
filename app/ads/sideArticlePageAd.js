472c5526d5b8ba14971ad3b085489866cb63f135
Felix La Rocque Carrier <felixlrc21@gmail.com>
2019-01-27 14:51:46 -0500

    Remove ID

--- app/ads/sideArticlePageAd.js
+++ app/ads/sideArticlePageAd.js
@@ -3,10 +3,8 @@ import React from 'react';
 import AD from 'react-google-publisher-tag';
 
 const SideArticlePageAd = () => (
-  <div style={{ textAlign: 'center' }}>
-    {typeof window !== 'undefined' && (
-      <AD path="/21784165674/article_side_ad" id="div-gpt-ad-1546916012144-0" />
-    )}
+  <div style={{ textAlign: 'center', minWidth: 300, minHeight: 250 }}>
+    {typeof window !== 'undefined' && <AD path="/21784165674/article_side_ad" />}
   </div>
 );
 
--- app/ads/sideDestinationPageAd.js
+++ app/ads/sideDestinationPageAd.js
@@ -3,10 +3,8 @@ import React from 'react';
 import AD from 'react-google-publisher-tag';
 
 const SideDestinationPageAd = () => (
-  <div style={{ textAlign: 'center' }}>
-    {typeof window !== 'undefined' && (
-      <AD path="/21784165674/destination_side_ad" id="div-gpt-ad-1546917788906-0" />
-    )}
+  <div style={{ textAlign: 'center', minWidth: 300, minHeight: 250 }}>
+    {typeof window !== 'undefined' && <AD path="/21784165674/destination_side_ad" />}
   </div>
 );
 
