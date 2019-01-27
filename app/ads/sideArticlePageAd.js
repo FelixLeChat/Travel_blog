dcc6a9ac039d8283247e588be3ebd5861667727b
Felix La Rocque Carrier <felixlrc21@gmail.com>
2019-01-27 14:14:58 -0500

    centered ads

--- app/ads/sideArticlePageAd.js
+++ app/ads/sideArticlePageAd.js
@@ -3,7 +3,11 @@ import React from 'react';
 import AD from 'react-google-publisher-tag';
 
 const SideArticlePageAd = () => (
-  <AD path="/21784165674/article_side_ad" id="div-gpt-ad-1546916012144-0" />
+  <div style={{ textAlign: 'center' }}>
+    {typeof window !== 'undefined' && (
+      <AD path="/21784165674/article_side_ad" id="div-gpt-ad-1546916012144-0" />
+    )}
+  </div>
 );
 
 export default SideArticlePageAd;
--- app/ads/sideDestinationPageAd.js
+++ app/ads/sideDestinationPageAd.js
@@ -3,7 +3,11 @@ import React from 'react';
 import AD from 'react-google-publisher-tag';
 
 const SideDestinationPageAd = () => (
-  <AD path="/21784165674/destination_side_ad" id="div-gpt-ad-1546917788906-0" />
+  <div style={{ textAlign: 'center' }}>
+    {typeof window !== 'undefined' && (
+      <AD path="/21784165674/destination_side_ad" id="div-gpt-ad-1546917788906-0" />
+    )}
+  </div>
 );
 
 export default SideDestinationPageAd;
